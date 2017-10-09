72c589cfcde5027589fe1114c7242aed95ae017b
DmitriC123 <cohan.dima@gmail.com>
2017-10-09 11:43:23 -0400

    Dog_class_set

---
+++ OOP_Practice/Dog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    class Dog
+    {
+        //fields
+        private string furLength;
+        private float height;
+        private string speed;
+        private int weight;
+
+        //Properties
+        public string FurLength
+        {
+            get { return this.furLength; }
+            set { this.furLength = value; }
+        }
+        public float Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public string Speed
+        {
+            get { return this.speed; }
+            set { this.speed = value; }
+        }
+        public int Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        //Constructors
+        public Dog()
+        {
+
+        }
+        
+        //overloaded Contructor
+        public Dog(string furLength, float height)
+        {
+            this.furLength = furLength;
+            this.height = height;
+        }
+    }
+}
+
