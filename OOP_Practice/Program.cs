fe3dc90fcff9bdf5e26c91eb955908d869024d51
DmitriC123 <cohan.dima@gmail.com>
2017-10-09 14:10:55 -0400

    Dog_Actions_Complete

--- OOP_Practice/Program.cs
+++ OOP_Practice/Program.cs
@@ -10,6 +10,40 @@ namespace OOP_Practice
     {
         static void Main(string[] args)
         {
+            //Dog firstDoge = new Dog();
+
+            //firstDoge.Weight = 60.00;
+            //firstDoge.Height = 3.2f;
+            //firstDoge.FurLength = "Flowing mane. ";
+            //firstDoge.Speed = "will fetch ... eventually. ";
+
+            //firstDoge.Groom();
+            //Console.WriteLine(firstDoge.FurLength);
+
+            //firstDoge.Run();
+            //Console.WriteLine("The dog now weighs " + firstDoge.Weight);
+            //Console.WriteLine(firstDoge.Speed);
+
+            Dog secondDoge = new Dog("fluffy ", 4.04f);
+            Console.WriteLine(secondDoge.FurLength);
+
+            secondDoge.Groom();
+            Console.WriteLine(secondDoge.FurLength);
+
+            secondDoge.Speed = "faster than lightning. ";
+            Console.WriteLine(secondDoge.Speed);
+
+            secondDoge.Run();
+            Console.WriteLine(secondDoge.Speed);
+
+            secondDoge.Weight = 78.9;
+            Console.WriteLine(secondDoge.Weight);
+
+            secondDoge.Potty();
+            Console.WriteLine(secondDoge.Weight);
+
+            secondDoge.Bark();
+
         }
     }
 }
